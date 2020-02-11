1acafc5657253a838d88648b9be8c0ec9ab1f781
费政聪 <37614046+feizc@users.noreply.github.com>
2020-02-11 15:37:00 +0800

    Add files via upload

---
+++ create_input_files.py
@@ -0,0 +1,6 @@
+from utils import create_input_files
+
+if __name__ == '__main__':
+    create_input_files(dataset='coco',karpathy_json_path='../cpation data/dataset_coco.json',
+                       image_folder='/media/ssd/caption data/',captions_per_image=5,
+                       min_word_freq=5,outputfolder='/media/ssd/caption data/',max_len=50)
