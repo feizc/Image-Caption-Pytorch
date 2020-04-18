37b3fb6f1ab3ebd9016bff6793c15f3ef69e9dfa
费政聪 <37614046+feizc@users.noreply.github.com>
2020-04-18 16:02:29 +0800

    Add files via upload

---
+++ code/create_input_files.py
@@ -0,0 +1,11 @@
+from utils import create_input_files
+
+if __name__ == '__main__':
+    # Create input files (along with word map)
+    create_input_files(dataset='coco',
+                       karpathy_json_path='../dataset_coco.json',
+                       image_folder='../data/',
+                       captions_per_image=5,
+                       min_word_freq=5,
+                       output_folder='../dataset/',
+                       max_len=50)
