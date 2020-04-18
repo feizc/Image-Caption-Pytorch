44940426037768dd63d72bb8ee853c5ee1652739
费政聪 <37614046+feizc@users.noreply.github.com>
2020-04-18 16:15:04 +0800

    Update train.py

--- code/train.py
+++ code/train.py
@@ -11,7 +11,7 @@ from utils import *
 from nltk.translate.bleu_score import corpus_bleu
 
 # Data parameters
-data_folder = '../dataset'  # folder with data files saved by create_input_files.py
+data_folder = '../data'  # folder with data files saved by create_input_files.py
 data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
 
 # Model parameters
